e82bec055225dfd660e5f216a8143fd94b39cfcb
ChrisKaldis <up1059364@upnet.gr>
2025-03-31 16:39:33 +0000

    start developing and testing the build_graph_coloring_bqm function.

--- graph_coloring.py
+++ graph_coloring.py
@@ -1,9 +1,68 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Christos Kaldis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import dimod
+import networkx as nx
 
 
-def build_graph_coloring_bqm() -> dimod.BinaryQuadraticModel:
-    """Constructs a BQM for the graph coloring problem."""
-    pass
+def build_graph_coloring_bqm(
+        graph: nx.graph,
+        num_colors: int,
+        a_coefficient: int = 2,
+        b_coefficient: int = 1,
+        ) -> dimod.BinaryQuadraticModel:
+    """Constructs a BQM for the graph coloring problem using QUBO formulation.
+
+    This function formulates the graph coloring problem as a Quadratic 
+    Unconstrained Binary Optimization (QUBO) problem, the formula is described 
+    briefly in the README.md file.
+
+    Args:
+        graph: NetworkX graph.
+        num_colors: Number of available colors.
+        a_coefficient: Penalty weight for single-color constraint.
+        b_coefficient: Penalty weight for adjacency constraint.
+
+    Returns:
+        dimod.BinaryQuadraticModel representing the graph coloting problem.
+    """
+    q_matrix = dict()
+    nodes = list(graph.nodes())
+    node_to_index = {node: i for i, node in enumerate(nodes)}
+    for node in nodes:
+        node_idx = node_to_index[node]
+        step_idx = node_idx * num_colors
+        # Single-color constraint,
+        for color in range(num_colors):
+            pos = step_idx + color
+            q_matrix[(pos,pos)] = -a_coefficient
+            for c in range(color+1, num_colors):
+                col_pos = node_idx * num_colors + c
+                q_matrix[(pos,col_pos)] = 2 * a_coefficient
+        # Adjacency constraint,
+        for neighbor in graph.neighbors(node):
+            neighbor_idx = node_to_index[neighbor]
+            if neighbor_idx > node_idx:
+                for color in range(num_colors):
+                    row_pos = step_idx + color
+                    col_pos = neighbor_idx * num_colors + color
+                    q_matrix[(row_pos, col_pos)] = b_coefficient
+
+    return dimod.BinaryQuadraticModel.from_qubo(q_matrix)
 
 
 def solve_graph_coloring(bqm: dimod.BinaryQuadraticModel):
@@ -13,4 +72,8 @@ def solve_graph_coloring(bqm: dimod.BinaryQuadraticModel):
 
 def visualize_results():
     """Plots the graph with node colors."""
+    pass
+
+
+if __name__ == "__main__":
     pass
---
+++ tests/test_build_graph_coloring_bqm.py
@@ -0,0 +1,99 @@
+import unittest
+import networkx as nx
+from graph_coloring import build_graph_coloring_bqm
+
+
+class TestBuildGraphColoringBQM(unittest.TestCase):
+
+    def setUp(self):
+        self.a = 2.0
+        self.b = 1.0
+
+    def assert_bqm_terms(self, bqm, expected_linear, expected_quadratic):
+        """Helper method to verify BQM terms"""
+        self.assertDictEqual(dict(bqm.linear), expected_linear)
+        self.assertDictEqual(dict(bqm.quadratic), expected_quadratic)
+
+    def test_single_node(self):
+        """Test graph with one node and no edges"""
+        G = nx.Graph()
+        G.add_node(0)
+        num_colors = 3
+
+        expected_linear_terms = {i: -self.a for i in range(num_colors)}
+
+        expected_quadratic_terms = {
+            (1, 0): 2 * self.a,
+            (2, 0): 2 * self.a,
+            (2, 1): 2 * self.a
+        }
+
+        bqm = build_graph_coloring_bqm(G, num_colors)
+
+        self.assert_bqm_terms(bqm, expected_linear_terms, expected_quadratic_terms)
+
+    def test_two_nodes_one_edge(self):
+        """Test graph with two nodes and one edge"""
+        G = nx.Graph([(0, 1)])
+        num_nodes = 2
+        num_colors = 2
+
+        expected_linear_terms = {i: -self.a for i in range(num_nodes * num_colors)}
+
+        expected_quadratic_terms = {
+            (1, 0): 2 * self.a,
+            (3, 2): 2 * self.a,
+            (2, 0): self.b,
+            (3, 1): self.b,
+        }
+
+        bqm = build_graph_coloring_bqm(G, num_colors)
+
+        self.assert_bqm_terms(bqm, expected_linear_terms, expected_quadratic_terms)
+
+
+    def test_triangle_graph(self):
+        """Test fully connected 3-node graph"""
+        G = nx.complete_graph(3)
+        num_nodes = 3
+        num_colors = 3
+
+        expected_linear_terms = {i: -self.a for i in range(num_nodes * num_colors)}
+
+        expected_quadratic_terms = {
+            (1, 0): 2 * self.a, (2, 0): 2 * self.a, (2, 1): 2 * self.a,
+            (4, 3): 2 * self.a, (5, 3): 2 * self.a, (5, 4): 2 * self.a,
+            (7, 6): 2 * self.a, (8, 6): 2 * self.a, (8, 7): 2 * self.a,
+            (3, 0): self.b, (4, 1): self.b, (5, 2): self.b,
+            (6, 0): self.b, (7, 1): self.b, (8, 2): self.b,
+            (6, 3): self.b, (7, 4): self.b, (8, 5): self.b
+        }
+
+        bqm = build_graph_coloring_bqm(G, 3)
+
+        self.assert_bqm_terms(bqm, expected_linear_terms, expected_quadratic_terms)
+
+    def test_disconnected_graph(self):
+        """Test graph with two disconnected components."""
+        G = nx.Graph([(0, 1), (2, 3)])
+        num_nodes = 4
+        num_colors = 3
+
+        expected_linear_terms = {i: -self.a for i in range(num_nodes * num_colors)}
+
+        expected_quadratic_terms = {
+            (1, 0): 2 * self.a, (2, 0): 2 * self.a, (2, 1): 2 * self.a,
+            (4, 3): 2 * self.a, (5, 3): 2 * self.a, (5, 4): 2 * self.a,
+            (7, 6): 2 * self.a, (8, 6): 2 * self.a, (8, 7): 2 * self.a,
+            (10, 9): 2 * self.a, (11, 9): 2 * self.a, (11, 10): 2 * self.a,
+            (3, 0): self.b, (4, 1): self.b, (5, 2): self.b,
+            (9, 6): self.b, (10, 7): self.b, (11, 8): self.b
+        }
+
+        bqm = build_graph_coloring_bqm(G, num_colors)
+
+        self.assert_bqm_terms(bqm, expected_linear_terms, expected_quadratic_terms)
+
+
+if __name__ == "__main__":
+    unittest.main()
